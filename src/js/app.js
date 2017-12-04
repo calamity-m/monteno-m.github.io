6002d952331c4ae892b66088ec6b0b8d39fd14da
monteno-m <markmonteno@gmail.com>
2017-12-05 07:49:51 +1100

    remove p a rticles
    removed particles test

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,3 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sweetScroll = new SweetScroll({/* some options */});
 
-  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS('particlessqsqss-js', , false);
