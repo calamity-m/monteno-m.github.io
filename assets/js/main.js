21277e45ef5dd41ab2788f03855d7925cdef573f
monteno-m <markmonteno@gmail.com>
2017-12-05 08:00:03 +1100

    Test
    etst

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const sweetScroll = new SweetScroll({/* some options */});
 
-particlesJS.load('particles-js', 'particlesjs.json', function() {
-  console.log('callback - particles.js config loaded');
-});
