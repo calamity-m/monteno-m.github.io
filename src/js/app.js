49812e6a6525af787b60722b73b1a585344af5ad
monteno-m <markmonteno@gmail.com>
2017-12-05 07:51:25 +1100

    Updated particles
    added particles back

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sweetScroll = new SweetScroll({/* some options */});
 
+particlesJS.load('particles-js', 'particlesjs.json', function() {
+  console.log('callback - particles.js config loaded');
+});
