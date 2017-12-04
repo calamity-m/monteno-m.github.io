41a27a96a470e9e7eaeace3a425a9b576485bc20
monteno-m <markmonteno@gmail.com>
2017-12-05 07:46:00 +1100

    Test
    t

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,4 @@ document.addEventListener("DOMContentLoaded", function () {
   const sweetScroll = new SweetScroll({/* some options */});
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS('particles-js', , false);
+  particlesJS('particlessqsqss-js', , false);
