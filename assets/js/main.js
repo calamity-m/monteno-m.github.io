0684fe8f725255d6cec0da239c3d5a36c37673c9
Mark M <markmonteno@gmail.com>
2017-12-05 12:00:26 +1100

    changed theme
    updated and changed themes

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-/* sweetScroll load */
-document.addEventListener("DOMContentLoaded", function () {
-  const sweetScroll = new SweetScroll({/* some options */});
-
-  particlesJS.load('particles-js', 'particleconfig.json', function() { }); 
-
-}, false);
